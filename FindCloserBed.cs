7e4e973dc835994eddd27e80c96ad2925901e253
Gamer_Z <githubio@gz0.nl>
2020-02-06 15:05:17 +0100

    Initial commit

---
+++ FindCloserBed.cs
@@ -0,0 +1,16 @@
+﻿using Harmony;
+using static ModLoader;
+
+namespace grasmanek94.FindCloserBed
+{
+    [ModManager]
+    public static class FindCloserBed
+    {
+        [ModCallback(EModCallbackType.OnAssemblyLoaded, "grasmanek94.FindCloserBed.OnAssemblyLoaded")]
+        static void OnAssemblyLoaded(string assemblyPath)
+        {
+            var harmony = HarmonyInstance.Create("grasmanek94.FindCloserBed");
+            harmony.PatchAll();
+        }
+    }
+}
---
+++ FindCloserBedHooks.cs
@@ -0,0 +1,22 @@
+﻿using BlockEntities.Implementations;
+using Harmony;
+using Pipliz;
+
+namespace grasmanek94.Statistics
+{
+	[HarmonyPatch(typeof(ColonyBeds))]
+	[HarmonyPatch("TryGetClosestUnused")]
+	public class ColonyBedsHookTryGetClosestUnused
+	{
+		static bool Prefix(ColonyBeds __instance, Vector3Int position, ref Vector3Int bedPosition, ref BedTracker.Bed bed, int boxradius)
+		{
+
+			return true;
+		}
+
+		static void Postfix(ColonyBeds __instance, bool __result, Vector3Int position, ref Vector3Int bedPosition, ref BedTracker.Bed bed, int boxradius)
+		{
+
+		}
+	}
+}
