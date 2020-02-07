44f98cd289c55ebe9ce15b45cd56d4ce33dab872
Gamer_Z <github@gz0.nl>
2020-02-07 16:01:05 +0100

    Search from job location

---
+++ ClassUtility.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+
+namespace grasmanek94.FindCloserBed
+{
+    public static class ClassUtility
+    {
+        public static void SetProperty(object instance, string propertyName, object newValue)
+        {
+            Type type = instance.GetType();
+
+            PropertyInfo prop = type.BaseType.GetProperty(propertyName);
+
+            prop.SetValue(instance, newValue, null);
+        }
+
+        public static void Call(object instance, string function, object[] parameters)
+        {
+            instance.GetType().GetMethod(function, BindingFlags.NonPublic | BindingFlags.Instance).Invoke(instance, parameters);
+        }
+    }
+}
