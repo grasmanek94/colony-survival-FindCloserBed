194a987faac54c3e13d877a42aeae2a88fb970a5
Gamer_Z <github@gz0.nl>
2020-02-07 16:23:42 +0100

    Searching from Job location moved to separate mod.

--- FindCloserBedHooks.cs
+++ FindCloserBedHooks.cs
@@ -1,8 +1,6 @@
 ﻿using BlockEntities.Implementations;
 using Harmony;
-using NPC;
 using Pipliz;
-using System.Reflection;
 
 namespace grasmanek94.FindCloserBed
 {
@@ -138,47 +136,6 @@ namespace grasmanek94.FindCloserBed
 			{
 				// Log.WriteWarning("   ... But game did not find a bed either");
 			}
-		}
-	}
-
-	[HarmonyPatch(typeof(NPCBase))]
-	[HarmonyPatch("CalculateGoalLocation")]
-	public class NPCBaseHookNPCGoal
-	{
-		static bool Prefix(NPCBase __instance, ref Vector3Int __result, NPCBase.NPCGoal goal)
-		{
-			switch (goal)
-			{
-				case NPCBase.NPCGoal.Bed:
-				{
-					if (__instance.UsedBed != null && __instance.UsedBed.IsValid)
-					{
-						return true;
-					}
-
-					if(__instance.Job == null)
-					{
-						return true;
-					}
-
-					Vector3Int jobLocation;
-					BedTracker.Bed bed;
-					if (__instance.Colony.BedTracker.TryGetClosestUnused(__instance.Job.GetJobLocation(), out jobLocation, out bed, 200))
-					{
-						Log.WriteWarning("Redirected NPC Goal");
-
-						ClassUtility.Call(__instance, "ClearBed", new object[] { });
-						ClassUtility.SetProperty(__instance, "UsedBed", bed);
-						ClassUtility.SetProperty(__instance, "UsedBedLocation", jobLocation);
-						bed.SetUseState(jobLocation, true);
-						__result =  jobLocation;
-						return false;
-					}
-					break;
-				}
-			}
-
-			return true;
 		}
 	}
 }
