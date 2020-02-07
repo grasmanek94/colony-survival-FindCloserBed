87cb5064d5e948d9cc24c12f57a78025a0bdd716
Gamer_Z <github@gz0.nl>
2020-02-07 16:24:11 +0100

    Update properties

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("grasmanek94.Statistics")]
+[assembly: AssemblyTitle("grasmanek94.FindCloserBed")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("grasmanek94.Statistics")]
+[assembly: AssemblyProduct("grasmanek94.FindCloserBed")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
